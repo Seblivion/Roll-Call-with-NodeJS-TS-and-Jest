ec2d7be236cd310ebe2c9169c6fd9834a152b8e4
Seb <Sebastian.Sjoberg.4578@student.uu.se>
2022-05-19 13:31:47 +0200

    Moved up the foreach in test file a step

--- src/test/CitizenListManager.test.ts
+++ src/test/CitizenListManager.test.ts
@@ -14,18 +14,7 @@ describe("This is testing ground for the citizen stack", () => {
         expect(citizenListManager.listToString()).toBe("1: Vlad Dracula, 2: Gustav Adolf");
     })
 
-    it("Testing the roll call function in the citizen list manager class", () => {
-        const listOfStudents = 
-            [new Student(1, "Will", "Smith"), 
-            new Student(2, "Agent", "Smith"),
-            new Student(3, "Peter", "Pan"),
-            new Student(4, "Micky", "Mouse"),
-            new Student(5, "Minnie", "Mouse"),
-            new Student(6, "Peter", "Gunn")];
-        const citizenListManager = new CitizenListManager(listOfStudents);
-        expect(citizenListManager.rollCall()).toEqual(["Peter Gunn", "Micky", "Minnie", "Peter Pan", "Agent", "Will"]);
-    })
-
+    // Create data for following tests
     var listOfStudents: Array<Student>;
     var citizenListManager: CitizenListManager;
     beforeEach(() => {
@@ -37,6 +26,10 @@ describe("This is testing ground for the citizen stack", () => {
             new Student(5, "Minnie", "Mouse"), //"5: Minnie Mouse"
             new Student(6, "Peter", "Gunn")]; //"6: Peter Gunn"
         citizenListManager = new CitizenListManager(listOfStudents);
+    })
+
+    it("Testing the roll call function in the citizen list manager class", () => {
+        expect(citizenListManager.rollCall()).toEqual(["Peter Gunn", "Micky", "Minnie", "Peter Pan", "Agent", "Will"]);
     })
 
     it("Testing the sorting by first name", () => {
