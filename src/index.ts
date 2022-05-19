f6afee20a83b40a01a67452f43f7a436ef44c700
Seb <Sebastian.Sjoberg.4578@student.uu.se>
2022-05-19 14:48:13 +0200

    Garbage user interaction with terminal

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-async function hello() {
-    return "hello";
+import * as readline from 'readline';
+
+import {CitizenListManager} from "./classes/CitizenListManager";
+import {Student} from "./classes/Student";
+
+function main() {
+    let stdin = process.openStdin();
+    let arrayWithNames: string[] = [];
+
+    console.log("Add names to list by writing first AND last name, press enter, and keep adding names until you are done.");
+    console.log("Write 'done' when finished.");
+    // Let user write input
+    stdin.addListener("data", function(objectToBeConvertedToString) {
+        if(objectToBeConvertedToString.toString().trim() == "done") {
+            let arrayOfStudents: Student[] = [];
+            for(let i = 0; i < arrayWithNames.length; i++) {
+                let tempArray: string[] = arrayWithNames[i].split(" ", 2);
+                arrayOfStudents.push(new Student(i, tempArray[0], tempArray[1]));
+            }
+            // Results
+            let citizenListManager = new CitizenListManager(arrayOfStudents);
+            console.log(citizenListManager.rollCall().toString());
+        }
+        else {
+            arrayWithNames.push(objectToBeConvertedToString.toString().trim());
+        }
+    });
 }
 
-/*const main = () => {
-    
-}*/
+main();
